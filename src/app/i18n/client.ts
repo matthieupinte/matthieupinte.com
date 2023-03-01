1276dbd6a2da985fbea4265c19776f10fdf1fa0f
Matthieu Pinte <matthieu@derniercri.io>
2023-03-01 18:55:59 +0100

    Update src/app/i18n/client.ts

--- src/app/i18n/client.ts
+++ src/app/i18n/client.ts
@@ -5,7 +5,6 @@ import { initReactI18next, useTranslation as useTranslationOrg } from 'react-i18
 import resourcesToBackend from 'i18next-resources-to-backend'
 import { getOptions } from './config'
 
-//
 i18next
   .use(initReactI18next)
   .use(resourcesToBackend((language: string, _namespace: string) => import(`./locales/${language}.json`)))
